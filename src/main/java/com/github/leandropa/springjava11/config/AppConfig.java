3006c4b35f5c2d7c80cdc4de7e398e50a410ed69
Leandro Pereira <leandroalencar@unigranrio.br>
2022-10-31 23:33:34 -0300

    Creating a util class for JWT

--- src/main/java/com/github/leandropa/springjava11/config/AppConfig.java
+++ src/main/java/com/github/leandropa/springjava11/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.github.leandropa.springjava11.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.leandropa.springjava11.util.JwtTokenUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,5 +17,10 @@ public class AppConfig {
 	@Bean
 	public ObjectMapper objectMapper() {
 		return new ObjectMapper();
+	}
+	
+	@Bean
+	public JwtTokenUtil jwtTokenUtil() {
+		return new JwtTokenUtil();
 	}
 }
---
+++ src/main/java/com/github/leandropa/springjava11/util/JwtTokenUtil.java
@@ -0,0 +1,28 @@
+package com.github.leandropa.springjava11.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.github.leandropa.springjava11.entity.User;
+
+import java.util.Date;
+
+public class JwtTokenUtil {
+
+	public String generateAccessToken(User user) {
+		return JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+				.sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
+	}
+
+	public boolean validate(String token) {
+		return false;
+	}
+
+	public String getUsername(String token) {
+		return JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()))
+				.build()
+				.verify(token)
+				.getSubject();
+	}
+}
