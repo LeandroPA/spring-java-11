1d37701d769fab194ba08f12d654d5f345e10fb4
Leandro Pereira <leandroalencar@unigranrio.br>
2022-11-04 04:51:36 -0300

    Changing to use Lombok

--- src/main/java/com/github/leandropa/springjava11/entity/User.java
+++ src/main/java/com/github/leandropa/springjava11/entity/User.java
@@ -1,12 +1,16 @@
 package com.github.leandropa.springjava11.entity;
 
-import org.bson.types.ObjectId;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Objects;
-
 @Document("users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class User {
 
 	@Id
@@ -16,51 +20,4 @@ public class User {
 
 	private String password;
 
-	public User() {
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		User user = (User) o;
-		return Objects.equals(username, user.username) && Objects.equals(password, user.password);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(username, password);
-	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-				"username='" + username + '\'' +
-				", password='" + password + '\'' +
-				'}';
-	}
 }
--- src/main/java/com/github/leandropa/springjava11/model/JwtResponse.java
+++ src/main/java/com/github/leandropa/springjava11/model/JwtResponse.java
@@ -1,16 +1,16 @@
 package com.github.leandropa.springjava11.model;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class JwtResponse implements Serializable {
 
 	private final String token;
 
-	public JwtResponse(String token) {
-		this.token = token;
-	}
-
-	public String getToken() {
-		return token;
-	}
 }
