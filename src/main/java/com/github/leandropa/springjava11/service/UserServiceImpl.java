e714d788430488b106a82e0f81b8ab232ee95c0c
Leandro Pereira <leandroalencar@unigranrio.br>
2022-11-07 15:11:45 -0300

    Using BCrypt for password

--- src/main/java/com/github/leandropa/springjava11/service/UserServiceImpl.java
+++ src/main/java/com/github/leandropa/springjava11/service/UserServiceImpl.java
@@ -3,18 +3,22 @@ package com.github.leandropa.springjava11.service;
 import com.github.leandropa.springjava11.entity.User;
 import com.github.leandropa.springjava11.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-	UserRepository userRepository;
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public UserServiceImpl(UserRepository userRepository) {
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
@@ -29,17 +33,23 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User insert(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 
 	@Override
 	public Optional<User> update(ObjectId id, User user) {
 		return getUserById(id)
-				.map(oldUser -> {
-					user.setId(oldUser.getId());
-					return user;
-				})
-				.map(newUser -> userRepository.save(newUser));
+				.map(oldUser -> mapUpdate(oldUser, user))
+				.map(userRepository::save);
+	}
+
+	private User mapUpdate(User oldUser, User newUser) {
+		newUser.setId(oldUser.getId());
+		if (!Objects.equals(oldUser.getPassword(), newUser.getPassword())) {
+			newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+		}
+		return newUser;
 	}
 
 	@Override
